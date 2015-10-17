1e771fe1dc9b925aa09832739ea4d2e9e1ba301f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-17 19:01:54 -0400

    added starfield and scrolling background

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+	// PUBLIC INSTANCE VARIABLES
+	public float scrollSpeed; 
+	public float tileSizeZ;
+
+	// PRIVATE INSTANCE VARIABLES
+	private Vector3  _startPosition;
+
+	// Use this for initialization
+	void Start () {
+		this._startPosition = gameObject.GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		float newPosition = Mathf.Repeat (Time.time * this.scrollSpeed, this.tileSizeZ);
+		gameObject.GetComponent<Transform> ().position = this._startPosition + Vector3.forward * newPosition;
+	}
+}
